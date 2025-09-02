40ce4aacc54c954eaf33158feca14f099bae2e6e
rahul <rahuldebnath39821@gmail.com>
2025-09-02 11:02:27 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e-commerce-site-for-order/' // <-- your repo name
+  base: './',   // ensures assets load correctly
 })
